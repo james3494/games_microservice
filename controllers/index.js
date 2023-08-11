b50f15ed0397869993e1b74f80c01043c2545a65
james7494 <james.mabon@btinternet.com>
2023-08-11 07:37:22 +0100

    update exising endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,25 +1,60 @@
-
-const { createTakeoutMethod, editTakeoutMethod } = require('../services');
-const { buildPostTakeoutMethod } = require('./postTakeoutMethod');
-const throwError = require('errorHandling').buildThrowError({ logErrors: process.env.LOG_ERRORS });
+const {
+  createTakeoutMethod,
+  editTakeoutMethod,
+  createGame,
+  editGame,
+} = require("../services");
+const { buildPostTakeoutMethod } = require("./postTakeoutMethod");
+const { buildPutTakeoutMethod } = require("./putTakeoutMethod");
+const { buildPostGame } = require("./postGame");
+const { buildPutGame } = require("./putGame");
+const throwError = require("errorHandling").buildThrowError({
+  logErrors: process.env.LOG_ERRORS,
+});
 
 const getLoggedIn = (httpRequest) => {
   try {
-    return JSON.parse(httpRequest.headers["X-Current-User"])
+    return JSON.parse(httpRequest.headers["X-Current-User"]);
   } catch (err) {
     throwError({
       title: "invalid user header passed",
       status: 500,
       error: "auth-invalid-user-header",
-      detail: "A stringified object should be passed by the gateway to the microservice in a X-Current-User header"
-    })
+      detail:
+        "A stringified object should be passed by the gateway to the microservice in a X-Current-User header",
+    });
   }
-}
+};
 
-const postTakeoutMethod = buildPostTakeoutMethod({ createTakeoutMethod, throwError, getLoggedIn });
+const postTakeoutMethod = buildPostTakeoutMethod({
+  createTakeoutMethod,
+  throwError,
+  getLoggedIn,
+});
+
+const putTakeoutMethod = buildPutTakeoutMethod({
+  editTakeoutMethod,
+  throwError,
+  getLoggedIn,
+});
+
+const postGame = buildPostGame({
+  createGame,
+  throwError,
+  getLoggedIn,
+});
+
+const putGame = buildPutGame({
+  editGame,
+  throwError,
+  getLoggedIn,
+});
 
 const takeoutMethodController = Object.freeze({
-  postTakeoutMethod
+  postTakeoutMethod,
+  putTakeoutMethod,
+  postGame,
+  putGame
 });
 
 module.exports = { ...takeoutMethodController };
--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,33 @@ const express = require('express');
 const api = express.Router();
 
 
-const { postTakeoutMethod, postGame } = require('../controllers');
+const { 
+    postTakeoutMethod, 
+    postGame,
+    putTakeoutMethod,
+    putGame
+} = require('../controllers');
 
 api.use(express.json());
-// todo change to unplurified words
 
-api.post( `${process.env.PATH_ROUTE}/takeoutMethods`, makeExpressCallback(postTakeoutMethod) );
+api.post( `${process.env.PATH_ROUTE}/takeoutMethod`, makeExpressCallback(postTakeoutMethod) );
+api.put( `${process.env.PATH_ROUTE}/takeoutMethod/:id`, makeExpressCallback(putTakeoutMethod) );
+
 api.post( `${process.env.PATH_ROUTE}/game`, makeExpressCallback(postGame) );
+api.put( `${process.env.PATH_ROUTE}/game/:id`, makeExpressCallback(putGame) );
 
-// put takeoutMethods/:id - edit a takeout method
 // get takeoutMethods - filters takeout methods
 // get takeoutMethods/:id - gets specific takeoutMethod
 // delete takeoutMethods/:id - disables a takeout method
-// put game/:id - edits a game
 // get game - filters games
 // get game/:id - gets specific game
-// put game/:id/invitees - edits the invitees
-// put game/:id/players - edits the players
-// put game/:id/admins - edits the admins
-// initiate a game - sometimes this will be an automatic thing at the start time, othertimes someone will click start
-// get game/:id/takeouts - filters takeouts
+// patch game/:id/invitees - edits the invitees
+// patch game/:id/players - edits the players
+// patch game/:id/admins - edits the admins
+// put game/:id/status initiate a game - sometimes this will be an automatic thing at the start time, othertimes someone will click start
+// get game/:id/takeouts - filters takeouts. Takeouts should only be got through a game
 // get game/:id/takeouts/:id - gets specific takeout
-// execute a takeout - someones successfully completed a takeout. this could either come from the target or chaser
-// finish game - this will either be called by someone or called from executing a takeout if it's the last one
+// put game/:id/takeouts/:id/status execute a takeout - someones successfully completed a takeout. this could either come from the target or chaser
+// put game/:id/status finish game - this will either be called by someone or called from executing a takeout if it's the last one
 
 module.exports = api;
